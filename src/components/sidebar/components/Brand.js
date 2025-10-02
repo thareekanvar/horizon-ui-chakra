ab075f041a957a3fca28a2fce95d9ddcbf2d71e6
Thareek Anvar M <thareekanvar@gmail.com>
2025-10-02 11:14:52 +0530

    Updated Brand.js

--- src/components/sidebar/components/Brand.js
+++ src/components/sidebar/components/Brand.js
@@ -1,19 +1,25 @@
 import React from "react";
 
 // Chakra imports
-import { Flex, useColorModeValue } from "@chakra-ui/react";
+import { Flex, Text, useColorModeValue } from "@chakra-ui/react";
 
 // Custom components
-import { HorizonLogo } from "components/icons/Icons";
 import { HSeparator } from "components/separator/Separator";
 
 export function SidebarBrand() {
   //   Chakra color mode
-  let logoColor = useColorModeValue("navy.700", "white");
+  let textColor = useColorModeValue("navy.700", "white");
 
   return (
     <Flex align='center' direction='column'>
-      <HorizonLogo h='26px' w='175px' my='32px' color={logoColor} />
+      <Text
+        fontSize='24px'
+        fontWeight='bold'
+        color={textColor}
+        my='32px'
+        letterSpacing='tight'>
+        DASHBOARD
+      </Text>
       <HSeparator mb='20px' />
     </Flex>
   );
